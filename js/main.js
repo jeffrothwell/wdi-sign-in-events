162858baefb8e921385993f6a250e4a82f8ad09a
jeffrothwell <rothwelljeff@gmail.com>
2018-02-12 14:36:37 -0500

    add error class to input fields on submit click, remove on focus

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', function(){
 
   var signInButtonEl = document.querySelector('.signin');
   var modalEl = document.querySelector('.modal');
-  var closeButtonEl = document.querySelector('.close')
+  var closeButtonEl = document.querySelector('.close');
+  var submitButtonEl = document.querySelector('.submit');
+  var userInputEl = document.querySelector('#user');
+  var passwordInputEl = document.querySelector('#pass');
 
   signInButtonEl.addEventListener('click', function(){
     modalEl.style.display = 'block'
@@ -11,5 +14,18 @@ document.addEventListener('DOMContentLoaded', function(){
   closeButtonEl.addEventListener('click', function(){
     modalEl.style.display = 'none'
   });
+
+  submitButtonEl.addEventListener('click', function(){
+    userInputEl.classList.add('error')
+    passwordInputEl.classList.add('error')
+  });
+
+  userInputEl.addEventListener('focus', removeErrors);
+
+  passwordInputEl.addEventListener('focus', removeErrors);
+
+  function removeErrors(e){
+    e.target.classList.remove('error');
+  }
 
 });
